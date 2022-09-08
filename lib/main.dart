e08b54da9ac899585de385b0698604ba07cb70a6
Mellenthin <nisse_5000@hotmail.com>
2022-09-09 01:09:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class Todo extends StatelessWidget {
   }
 }
 
+// ignore: use_key_in_widget_constructors
 class TodoList extends StatefulWidget {
   @override
+  // ignore: library_private_types_in_public_api
   _TodoListState createState() => _TodoListState();
 }
 
@@ -21,7 +23,14 @@ class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('TIG333 TODO')),
+      appBar: AppBar(
+          backgroundColor: Colors.grey[400],
+          centerTitle: true,
+          title: const Text(
+            'TIG333 TODO',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black),
+          )),
       body: ListView(
         padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
         children: <Widget>[
@@ -34,13 +43,103 @@ class _TodoListState extends State<TodoList> {
                   Text('Write a book',
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
                   Icon(Icons.close)
                 ],
               )),
           Container(
-              height: 50, color: Colors.grey[100], child: Text('Do homework')),
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Do homework',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
           Container(
-              height: 50, color: Colors.grey[100], child: Text('Tidy room')),
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box),
+                  Text('Tidy room',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          decoration: TextDecoration.lineThrough)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
+          Container(
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Watch TV',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
+          Container(
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Nap',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
+          Container(
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Shop groceries',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
+          Container(
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Have fun',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
+          Container(
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Text('Meditate',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Spacer(),
+                  Icon(Icons.close)
+                ],
+              )),
         ],
       ),
     );
