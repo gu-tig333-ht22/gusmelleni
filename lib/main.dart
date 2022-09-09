bbadd995c13ab42af7d43c7f70747db1b21e2bab
Mellenthin <nisse_5000@hotmail.com>
2022-09-09 14:53:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,118 +33,238 @@ class _TodoListState extends State<TodoList> {
           title: const Text(
             'TIG333 TODO',
             style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black),
+                fontWeight: FontWeight.w500, fontSize: 28, color: Colors.black),
           )),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => AddToDo()));
+        },
+        label: const Text('Add'),
+        icon: Icon(Icons.add),
+        backgroundColor: Colors.grey[400],
+      ),
       body: ListView(
-        padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
         children: <Widget>[
           Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
                   Icon(Icons.check_box_outline_blank),
-                  Text('Write a book',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Write a book',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
           Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
                   Icon(Icons.check_box_outline_blank),
-                  Text('Do homework',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Do homework',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
           Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
-                  Icon(Icons.check_box),
-                  Text('Tidy room',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                          decoration: TextDecoration.lineThrough)),
+                  Icon(Icons.check_box_outlined),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Tidy room',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 28,
+                            decoration: TextDecoration.lineThrough)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
           Container(
-              height: 50,
-              color: Colors.grey[100],
-              child: Row(
-                children: [
-                  Icon(Icons.check_box_outline_blank),
-                  Text('Watch TV',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                  Spacer(),
-                  Icon(Icons.close)
-                ],
-              )),
-          Container(
-              height: 50,
-              color: Colors.grey[100],
-              child: Row(
-                children: [
-                  Icon(Icons.check_box_outline_blank),
-                  Text('Nap',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                  Spacer(),
-                  Icon(Icons.close)
-                ],
-              )),
-          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
                   Icon(Icons.check_box_outline_blank),
-                  Text('Shop groceries',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Watch TV',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
           Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
                   Icon(Icons.check_box_outline_blank),
-                  Text('Have fun',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Nap',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
           Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
               height: 50,
               color: Colors.grey[100],
               child: Row(
                 children: [
                   Icon(Icons.check_box_outline_blank),
-                  Text('Meditate',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Shop groceries',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
                   Spacer(),
-                  Icon(Icons.close)
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
+                ],
+              )),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Have fun',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
+                ],
+              )),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              height: 50,
+              color: Colors.grey[100],
+              child: Row(
+                children: [
+                  Icon(Icons.check_box_outline_blank),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(22, 0, 0, 0),
+                    child: Text('Meditate',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 28)),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Icon(Icons.close),
+                  ),
                 ],
               )),
         ],
+      ),
+    );
+  }
+}
+
+class AddToDo extends StatelessWidget {
+  const AddToDo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black)),
+        backgroundColor: Colors.grey[400],
+        centerTitle: true,
+        title: const Text(
+          'TIG333 TODO',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        alignment: AlignmentDirectional.topCenter,
+        child: Column(
+          children: <Widget>[
+            const TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(20),
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2.0),
+                    ),
+                    labelText: 'What are you going to do?')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 47, 0, 0),
+                  child: Icon(Icons.add),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 47, 0, 0),
+                  child: Text('ADD',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 16)),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
